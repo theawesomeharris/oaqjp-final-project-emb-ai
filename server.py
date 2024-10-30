bd572183586cbbfd25a012cdf9c0f01110b1aa3b
jxsaw <jxsaw@hotmail.com>
2024-10-30 08:31:40 -0400

    performed static code analysis using pylint for a pylint score of 10/10

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
+''' Executing this function initiates the application of emotion analyzer 
+    to be executed over the Flask channel and deployed on localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    This code receives the text from the HTML interface and 
+    runs emotion detector over it using emotion_detector() function
+    The output returned shows the scores of each emotion detected and
+    the dominant emotion.
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -15,14 +24,17 @@ def sent_analyzer():
 
     if dominant_emotion == "None":
         return "Invalid text! Please try again!"
-    else:
-        return f"For the given statement, the system response is 'anger': {anger}, \
-        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
-        The dominant emotion is {dominant_emotion}."
+
+    return f"For the given statement, the system response is 'anger': {anger}, \
+    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
+    The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
