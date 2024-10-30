faa4c5f8a8bbf12006b2c6e4b54ed4d2773a6d3e
jxsaw <jxsaw@hotmail.com>
2024-10-30 07:59:14 -0400

    incorporated error handling for status code 400 and 500

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,13 +7,23 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url,json=myobj,headers=header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
-    dominant_emotion = max(emotions,key=emotions.get)
+
+    if response.status_code==200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+        dominant_emotion = max(emotions,key=emotions.get)
+    elif response.status_code == 400 or response.status_code == 500:
+        anger_score = "None"
+        disgust_score = "None"
+        fear_score = "None"
+        joy_score = "None"
+        sadness_score = "None"
+        emotions = "None"
+        dominant_emotion = "None"       
     return  {
         'anger': anger_score,
         'disgust': disgust_score,
